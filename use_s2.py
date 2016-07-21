b7dbd463a7286a653cf247a53a4c0e0af170fb22
blairwhittington <blairjw@umich.edu>
2016-07-21 10:05:58 -0400

    Updated use_s2.py to fit the updated class

--- use_s2.py
+++ use_s2.py
@@ -3,8 +3,10 @@ from s2_calculator import s2_calculator
 import MDAnalysis 
 
 u = MDAnalysis.Universe("1R7Z.nomin.psf", "pool.dcd")
-t = ("C8", "H8")
+t = ("C1'", "H1'")
 nframes = len(u.trajectory)
+nresid = len(u.atoms.residues)
 s2 = s2_calculator(u, t)
 s2.get_all_s2()
 s2.get_scatterplot()
+s2.get_table()
