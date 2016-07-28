7611182b478eccf6847822fcfacfbbec406396cb
blairwhittington <blairjw@umich.edu>
2016-07-28 11:11:00 -0400

    Bond Vector Selection, Alignment, and Table
    I was able to get the alignment and bond vector selection functions to
    work properly. I also fixed my previous get_table function so that it
    prints the table of resid, s2 order parameters, and bond vector
    selections. These columns are also aligned so that the output is easier
    to read.

--- s2_calculator.py
+++ s2_calculator.py
@@ -3,54 +3,46 @@ import MDAnalysis
 
 def initialize_universe(PSF, DCD0, align = True, REF = "reference.pdb", DCD1 = "pool_align.dcd"):
     """ A method to set up universe for S2 calculation;
-    returns a universe that may or not be aligned to some reference"""
-
-		# load initial universe
-		u = MDAnalysis.Universe(PSF, DCD0)
-
-		# Test if alignment is requested; default is yes!
-		# align trajectory
-		write_reference(u, REF, i=0) 
-
-		# reads in reference
-		ref = Universe(PSF, REF)
-		rms_fit_trj(u, ref, filename=DCD1)
-
-		# read in new universe with aligned trajectory file
-		u = MDAnalysis.Universe(PSF, DCD1)
-		
-		return u
+    returns a universe that may or may not be aligned to some reference"""
+    from MDAnalysis.analysis.align import *
+    # Load initial universe
+    u = MDAnalysis.Universe(PSF, DCD0)
+    # Test if alignment is requested; default is yes!
+    # Align trajectory
+    write_reference(u, REF, i=0) 
+    # Reads in reference pdb
+    ref = MDAnalysis.Universe(PSF, REF)
+    trj = u
+    rms_fit_trj(trj, ref, filename=DCD1)
+    # Read in new universe with aligned trajectory file
+    u = MDAnalysis.Universe(PSF, DCD1)
+    return u
 
 def write_reference(u, output_name, i=0):
-  """ this function write out a specific frame in a universe"""
-	u.trajectory[i]
-	u.atoms.write(output_name)
-	
-
+    """ This function writes out a specific frame in a universe"""
+    u.trajectory[i]
+    u.atoms.write(output_name)
+    
 class s2_calculator:
     """A method for initializing s2 order parameter, and residue number"""
     def __init__(self, u, t):
-        """Initializing the class variable that will be used later"""
+        """Initializing the class variables that will be used later"""
         self.u = u
         self.t = t
         self.s2_list = []
         self.resid_list = []
-        self.carbon_list = []
-        self.hydrogen_list = []
         self.nframes = len(self.u.trajectory)
-	    self.nresid = len(self.u.atoms.residues)
-	
-    def select_bond_vector(self, i):
-		"""A method for selecting a single bond vector"""
-		for i in self.t:
-			self.sel1 = self.u.select_atoms("resid %s and name %s" % (i, self.t[0]))  
-			self.sel2 = self.u.select_atoms("resid %s and name %s" % (i, self.t[1])) 
-			
-			if self.sel1.n_atoms != 1 and self.sel2.n_atoms != 1:
-				return False
-			else:
-				return True
-						
+        self.nresid = len(self.u.atoms.residues)
+    
+    def select_bond_vector(self, i=1):
+        """A method for selecting a single bond vector"""
+        self.sel1 = self.u.select_atoms("resid %s and name %s" % (i, self.t[0]))  
+        self.sel2 = self.u.select_atoms("resid %s and name %s" % (i, self.t[1])) 
+        if self.sel1.n_atoms != 1 and self.sel2.n_atoms != 1:
+            return False
+        else:
+            return True
+            
     def get_s2(self, i=1):
         """A method for computing s2 order parameter for one residue"""
         # Set initial vector quantities equal to zero 
@@ -59,42 +51,38 @@ class s2_calculator:
         z2 = 0
         xy = 0
         xz = 0
-        yz = 0
-        
+        yz = 0   
         # Check selection        
         s2 = -1
         sele = self.select_bond_vector(i)        
         if sele:
-					# Loop over trajectory
-					for ts in self.u.trajectory:
-							# Define vector CH
-							vecCH = sel1.center_of_mass() - sel2.center_of_mass()
-							import numpy
-							vecCH = self.normalize_vec(vecCH) 
-							# Get vector components
-							xcomp = vecCH[0]
-							ycomp = vecCH[1]
-							zcomp = vecCH[2]     
-						
-							# Iterate through vector components
-							x2 += xcomp * xcomp
-							y2 += ycomp * ycomp
-							z2 += zcomp * zcomp
-							xy += xcomp * ycomp
-							xz += xcomp * zcomp
-							yz += ycomp * zcomp
-									
-					# Calculate vector averages
-					x2 = x2 / self.nframes
-					y2 = y2 / self.nframes
-					z2 = z2 / self.nframes
-					xy = xy / self.nframes
-					xz = xz / self.nframes
-					yz = yz / self.nframes
-					# Calculate s2 
-					s2 = (1.5 * ((x2 ** 2) + (y2 ** 2) + (z2 ** 2))) + (3 * ((xy ** 2) + (xz ** 2) + (yz ** 2))) - 0.5
-			  return s2
-
+            # Loop over trajectory
+            for ts in self.u.trajectory:
+                # Define vector CH
+                vecCH = self.sel1.center_of_mass() - self.sel2.center_of_mass()
+                import numpy
+                vecCH = self.normalize_vec(vecCH) 
+                # Get vector components
+                xcomp = vecCH[0]
+                ycomp = vecCH[1]
+                zcomp = vecCH[2]     
+                # Iterate through vector components
+                x2 += xcomp * xcomp
+                y2 += ycomp * ycomp
+                z2 += zcomp * zcomp
+                xy += xcomp * ycomp
+                xz += xcomp * zcomp
+                yz += ycomp * zcomp                                  
+            # Calculate vector averages
+            x2 = x2 / self.nframes
+            y2 = y2 / self.nframes
+            z2 = z2 / self.nframes
+            xy = xy / self.nframes
+            xz = xz / self.nframes
+            yz = yz / self.nframes
+            # Calculate s2 
+            s2 = (1.5 * ((x2 ** 2) + (y2 ** 2) + (z2 ** 2))) + (3 * ((xy ** 2) + (xz ** 2) + (yz ** 2))) - 0.5
+            return s2
             
     # A method for computing s2 order parameters for all residues
     def get_all_s2(self):
@@ -120,25 +108,14 @@ class s2_calculator:
         pyplot.ylabel('s2 Order Parameter')
         # Show the plot 
         pyplot.show()
-                  
+        
     def get_table(self):
-        """A method for listing s2 order parameters by resids and bond vectors in a table"""
-        from astropy.table import Table, Column
-        import numpy as np
-        from itertools import repeat
-        # Define a column resid that stores information from self.resid_list
-        resid = self.resid_list
-        # Define a column s2 that stores information from self.s2_list
-        s2 = self.s2_list
-        # Define carbon as a list containing carbon atom selection repeated once for each residue
-        self.carbon_list.extend(repeat(self.t[0], self.nresid))
-        carbon = self.carbon_list
-        # Define hydrogen as a list containing hydrogen atom selection repeated once for each residue 
-        self.hydrogen_list.extend(repeat(self.t[1], self.nresid))
-        hydrogen = self.hydrogen_list
-        # Create a table containing resid, s2 order parameter, carbon and hydrogen atoms selection information 
-        s = Table([resid, s2, carbon, hydrogen], names = ("resid", "s2", "carbon_selection", "hydrogen_selection"))
-        s
+        """ A method for printing a table of resids, s2 order parameters, and 
+        bond vector selections"""
+        self.s2.s2_list = [ '%.8f' % i for i in self.s2.s2_list]
+        self.s2.resid_list = [ '%.2i' % i for i in self.s2.resid_list]
+        for i, v in zip(self.s2.resid_list, self.s2.s2_list):
+            print i , v , self.t[0], self.t[1]
         
     # Normalize vectorCH  
     def norm_vec(self, v):
--- use_s2.py
+++ use_s2.py
@@ -1,16 +1,23 @@
 import s2_calculator
-from s2_calculator import s2_calculator
+from s2_calculator import *
 import MDAnalysis 
 
 PSF = "1R7Z.nomin.psf"
-DCD = "pool.dcd"
-u = initialize_universe(PSF, DCD)
+DCD0 = "pool.dcd"
+u = initialize_universe(PSF, DCD0)
 
 t = ("C1'", "H1'")
+
 nframes = len(u.trajectory)
 nresid = len(u.atoms.residues)
 s2 = s2_calculator(u, t)
 s2.get_all_s2()
 print s2.s2_list
 s2.get_scatterplot()
-s2.get_table()
+
+s2.resid_list = [ '%.2i' % i for i in s2.resid_list]
+s2.s2_list = [ '%.8f' % i for i in s2.s2_list]
+for i, v in zip(s2.resid_list, s2.s2_list):
+	print i , v , t[0], t[1]
+	
+
