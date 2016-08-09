bebb6613890a83ac6a2fa10f96f0988cbb11373c
blairwhittington <blairjw@umich.edu>
2016-08-09 10:08:16 -0400

    Debugging functions that compute ontact sum
    These are the most recent files for the primary and alternative contact
    sum functions. The functions are currently not computing the same sum.

--- contact_sum.py
+++ contact_sum.py
@@ -1,59 +1,25 @@
-def get_contact_sum(self, i=1):
-	""" A method for computing contact sum"""
-	import MDAnalysis.lib.distances
-	from MDAnalysis.lib.distances import distance_array 
-	self.r_eff = 5.0
-	self.r_cut = 5.0
-	reference = self.u.select_atoms("resid %s and name %s" % (i, self.t[0]))			
-	close_contacts = self.u.select_atoms("(around 5.0 resid %s and name %s) and (not resid %s) and (not type hydrogen)" % (i, self.t[0], i))		
-	r_ij = distance_array(reference.coordinates(), close_contacts.coordinates())
-	Ci = numpy.sum(numpy.exp(-1 * (r_ij / r_eff)))
-	return Ci
+class contact_sum:
+	def __init__(self, u, t):
+		"""Initializing the class variables that will be used later"""
+ 		self.u = u
+ 		self.t = t
+ 		
+ 	def get_contact_sum(self, i=1, r_eff=5.0, r_cut=5.0):
+		"""A method for computing contact sum"""
+		# Load necessary modules
+		import MDAnalysis.lib.distances
+		from MDAnalysis.lib.distances import distance_array
+		import numpy as numpy
+		# Select carbon atom 
+		reference = self.u.select_atoms("resid %s and name %s" % (i, self.t[0]))
+		# Select all atoms within 5.0A of carbon atom selection            
+		close_contacts = self.u.select_atoms("(around %f resid %s and name %s) and (not resid %s) and (not type 102 103 104 109 111 112)" % (r_cut, i, self.t[0], i))      
+		# Compute the distances between reference and close_contacts coordinates
+		r_ij = distance_array(reference.coordinates(), close_contacts.coordinates())
+		# Compute contact sum
+		Ci = numpy.sum(numpy.exp(-1 * (r_ij / r_eff)))
+		print Ci 
 	
-	
-	
-	
-- create an array that contains distances between all atom distance coordinates of u
-- create a function that can identify the index of carbon atom selection
-- use the carbon atom index to slice the array and get distances
-- loop over the array 
-- accumulate sums 
-	
-### Alternative method for computing contact sum
-def get_carbon_atom_index(self):
-	""" An Alternative method for computing contact sum"""
-	dismat = distance_array(self.u.atoms.coordinates(), self.u.atoms.coordinates())
-	# Find carbon atom selection index 
-	for i in self.u.select_atoms("name C1'"):
-		print i
-	# Use carbon atom index to slice array
-	dismat[8]
-	# Get distances
-	carbon_dist = distance_array(reference.coordinates(),close_contacts.coordinates())
-	# Get contact sum
-	Ci = numpy.sum(numpy.exp(-1 * (carbon_dist / r_eff)))
-	return Ci
-	
-	
-	
-	
-	
-	
-	for i, j in enumerate(dismat):
-		if j == "C1'":
-			print i
-			
-	print([atom.index for atom in u.atoms if atom.element.symbol is "C1'"])
-	
-	for i in u.select_atoms("resid %s and name C1'" % (i)):
-		print i
 
-	u.select_atoms("name C1'").coordinates()
+
 	
-	for i in dismat[8]:
-		if i < 5.0:
-			print i
-			
-	for i in dismat[8]:
-         if i < 5.0:
-   					array.append(i)
--- use_s2.py
+++ use_s2.py
@@ -1,19 +1,27 @@
 import s2_calculator
 from s2_calculator import *
+import contact_sum
+from contact_sum import *
+#import contact_sum_method_2
+#from contact_sum_method_2 import *
 import MDAnalysis 
 
 PSF = "1R7Z.nomin.psf"
 DCD0 = "pool.dcd"
+
 u = initialize_universe(PSF, DCD0)
 
 t = ("C1'", "H1'")
 
 nframes = len(u.trajectory)
 nresid = len(u.atoms.residues)
+
+
 s2 = s2_calculator(u, t)
 s2.get_all_s2()
 print s2.s2_list
 s2.get_scatterplot()
+
 
 s2.resid_list = [ '%.2i' % i for i in s2.resid_list]
 s2.s2_list = [ '%.8f' % i for i in s2.s2_list]
@@ -21,3 +29,9 @@ for i, v in zip(s2.resid_list, s2.s2_list):
 	print i , v , t[0], t[1]
 	
 
+sum = contact_sum(u, t)
+sum.get_contact_sum()
+
+#sum = contact_sum(u, t)
+#sum.get_carbon_atom_index()
+#sum.get_contact_sum()
