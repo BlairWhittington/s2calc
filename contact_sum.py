75908bf43ad6ec2d2c7e4322c4c7292f4e935800
blairwhittington <blairjw@umich.edu>
2016-08-25 12:16:30 -0400

    Testing if reference atom in bond vector selection exists.

--- contact_sum.py
+++ contact_sum.py
@@ -11,34 +11,32 @@ class contact_sum:
         self.r_cut = r_cut
         self.r_eff = r_eff
         self.Ci_list = []
-        self.resid_list = []
-                         
-    def get_contact_sum(self, i, l):
+                   
+    def get_contacts(self, i , l):
         """A method for computing contact sum"""
         # Assign reference to first atom selection
-        reference = self.u.select_atoms("resid %s and name %s" % (i, l[0]))  
-        # Select all atoms within 5.0A of first atom selection       
-        try:
-          close_contacts = self.u.select_atoms("(around %f resid %s and name %s) and ((not resid %s) and (not name H*))" % (self.r_cut, i, l[0], i)) 
-          # Compute the distances between reference and close_contacts coordinates
-          r_ij = distance_array(reference.coordinates(), close_contacts.coordinates())
-          #Compute contact sum
-          Ci = numpy.sum(numpy.exp(-1 * (r_ij / self.r_eff)))         
-        except:
-          print l[0], i
-          Ci = -1
+        self.reference = self.u.select_atoms("resid %s and name %s" % (i, l[0]))
+        # Select all atoms within 5.0A of first atom selection    
+        self.close_contacts = self.u.select_atoms("(around %f resid %s and name %s) and ((not resid %s) and (not name H*))" % (self.r_cut, i, l[0], i))
+        # Check to see if selections exist
+        if self.reference.n_atoms != 1:
+            return False
+        else:
+            return True
+            
+    def get_contact_sum(self, i, l):
+        """A method for checking if selection exists"""
+        Ci = -1 
+        sele = self.get_contacts(i, l)
+        print sele
+        if sele:
+            r_ij = distance_array(self.reference.coordinates(), self.close_contacts.coordinates())
+            Ci = numpy.sum(numpy.exp(-1 * (r_ij / self.r_eff))) 
         return Ci
-        
+    
     def get_all_contact_sums(self):
         """A method for looping over residues to compute all contact sums"""
         for l in self.t:
-          for i in self.u.atoms.residues.resids:
-          #for i in self.u.select_atoms("name %s" % (self.t[1])).residues.resids:
-            self.Ci_list.append(self.get_contact_sum(i, l)) 
-            self.resid_list.append(i)
-    
- 
- 
-    
-
+            for i in self.u.atoms.residues.resids:
+                self.Ci_list.append(self.get_contact_sum(i, l)) 
 
