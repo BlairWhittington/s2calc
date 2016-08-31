454d8539936251784e28b7bd6f771da826efa3ba
atfrank <afrankz@umich.edu>
2016-08-31 13:31:58 -0400

    started debugging code...
    — seems to be an error in the stacking and hydrogen bonding calculations

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,8 +1,24 @@
-def test_alignment(PSF, DCD0, align = True, REF = "reference.pdb", DCD1 = "pool_align.dcd"):
-    """ A method to test the alignment function"""
-    from MDAnalysis.analysis.align import *
-    # Load initial universe
-    u = MDAnalysis.Universe(PSF, DCD0)
-    # Test if alignment is requested; default is yes!
-    # Align trajectory
-    write_reference(u, REF, i=0) 
+import s2_calculator
+from s2_calculator import *
+import contact_sum
+from contact_sum import *
+import MDAnalysis 
+from itertools import repeat
+from optparse import OptionParser
+import warnings, sys
+import Features
+from Features import Features
+
+#initialize variables from the commandline
+
+PSF="/Users/atfrank/GitSoftware/s2calc/data/2l3e/reference.psf"
+DCD0="/Users/atfrank/GitSoftware/s2calc/data/2l3e/test.dcd"
+universe = initialize_universe(PSF, DCD0)
+new = Features(universe)
+for ts in universe.trajectory:
+		new.ts = ts.frame-1
+		new.loadCoordinateDependentDataStructures()
+		new.residueLevelFeaturesCompute()
+
+
+647-3983
