379d09f5da95c48797f8e158e52ae216e36e9349
blairwhittington <blairjw@umich.edu>
2017-02-06 10:43:54 -0500

    Updated Machine Learning Codes and Saved Linear Regression Models
    -Saved regression models using data for 10A, 15A, 20A, and 25A cutoffs
    & 60:40 Training: Testing

--- scripts/code_snippet.py
+++ scripts/code_snippet.py
@@ -2,6 +2,7 @@ from sklearn.externals import joblib # joblib
 from sklearn import linear_model
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
+import numpy as np
 
 # Load dataset
 dataset = np.loadtxt("10.0.txt", usecols = range(1, 80))
@@ -10,26 +11,28 @@ dataset = np.loadtxt("20.0.txt", usecols = range(1, 80))
 dataset = np.loadtxt("25.0.txt", usecols = range(1, 80))
 y = dataset[:, 0]
 x = dataset[:, 1:]
+y.shape, x.shape 
 		
 # Split into training and testing sets				
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.4, random_state=0) 
 x_train.shape, y_train.shape
+x_test.shape, y_test.shape
+
+# Linear Regression
+reg = linear_model.LinearRegression()
+reg.fit(x_train, y_train)
+# Save model
+joblib.dump(reg, "LR_10.0.pkl", compress=3)
+joblib.dump(reg, "LR_15.0.pkl", compress=3)
+joblib.dump(reg, "LR_20.0.pkl", compress=3)
+joblib.dump(reg, "LR_25.0.pkl", compress=3)
 
 # Set parameters for RF and ERT
 max_features = 1.0
 min_samples_leaf = 5
 min_samples_split = 5
 n_estimators = 1000
-
-# Linear Regression
-regr = linear_model.LinearRegression()
-regr.fit(x_train, y_train)
-# Save model
-joblib.dump(reg, "LR_10.0.pkl", compress=3)
-joblib.dump(reg, "LR_15.0.pkl", compress=3)
-joblib.dump(reg, "LR_20.0.pkl", compress=3)
-joblib.dump(reg, "LR_25.0.pkl", compress=3)
 
 # RF
 reg = RandomForestRegressor(bootstrap=True,criterion='mse', max_depth=None, max_features=max_features, min_samples_leaf=min_samples_leaf, min_samples_split=min_samples_split,n_estimators=n_estimators, n_jobs=1, oob_score=True, random_state=None,verbose=0)
