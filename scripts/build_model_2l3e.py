f79408b490a8d5e31efc720d6b054a7f7ea54cf8
blairwhittington <blairjw@umich.edu>
2017-04-03 10:38:45 -0400

    Most updated files for 2l3e project
    -Includes data for 10 and 15A cutoffs:
    *files contain information about predicted and computed s2 order
    parameters
    *plots of s2 order parameters (predicted and computed) vs. resid index

--- scripts/build_model_2l3e.py
+++ scripts/build_model_2l3e.py
@@ -5,9 +5,9 @@ from sklearn import linear_model
 from sklearn.externals import joblib
 
 ### Load in training set which contains information for all RNAs but 2l3e ###	
-training = np.loadtxt("10.0.train.txt", usecols = range(1, 80))	
+training = np.loadtxt("15.0.train.txt", usecols = range(1, 80))	
 ### Load in testing set which contains information for only 2l3e ###
-testing = np.loadtxt("10.0.2l3e.txt", usecols = range(1,80))
+testing = np.loadtxt("15.0.2l3e.txt", usecols = range(1,80))
 ### Divide training and testing sets into s2 order parameters and features ###
 y_train = training[:, 0] #s2 order parameters for all RNAs expect 2l3e
 x_train = training[:, 1:] #features for all RNAs except 2l3e
@@ -26,11 +26,11 @@ print(reg.score(x_test, y_test))
 ### Compute feature importances ###
 print(reg.coef_)
 ### Predict s2 order parameters ###
-print reg.predict(x_test) #predicted s2 values for 2l3e only  
-s2 = reg.predict(x_test)
+s2 = reg.predict(x_test) #predicted s2 values for 2l3e only  
 s2.tolist()
+print s2
 ### Save predictive model ###
-joblib.dump(reg, "LR_10.0A_2l3e.pkl", compress=3) 
+joblib.dump(reg, "LR_15.0A_2l3e.pkl", compress=3) 
 
 ### Plotting s2 Order Parameters vs Residue Index ###
 #Could import txt.files with data, plot all 7 bond vectors and complete for 15A, 20A, and 25A
@@ -55,14 +55,12 @@ plt.show()
 ### Plotting change in s2 Order Parameters vs Residue Index ###
 import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
-x1 = resid_index
-x2 = 0
-y3 = 0
+x = resid_index
 y = np.subtract(y2, y1)
 y = y.tolist()
 plt.title('Difference in s2 Order Parameters vs. Residue Index')
-plt.plot(x1,y, marker='o', linestyle='-', color='b')
-plt.plot(x1*y3, marker='None', linestyle='-', color='r')
+plt.plot(x,y, marker='o', linestyle='-', color='b')
+plt.plot(x,y3, marker='None', linestyle='-', color='r')
 blue_line = mlines.Line2D([],[], color='blue', marker='o', markersize=8, label='Actual - Predicted s2 Order Parameters')
 plt.legend(handles=[blue_line])
 plt.xlabel('Residue Index')
